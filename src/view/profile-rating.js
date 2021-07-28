95066821d87a66a86a8c672d168f50a0efb689e2
Dimahtml <bomabo@mail.ru>
2021-07-28 21:10:14 +0500

    создал файлы для остальных компонент

---
+++ src/view/profile-rating.js
@@ -0,0 +1,7 @@
+/* eslint-disable semi */
+export const createFilmCardTemplate = () => {
+  `<section class="header__profile profile">
+    <p class="profile__rating">Movie Buff</p>
+    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+  </section>`
+};
---
+++ src/view/show-more.js
@@ -0,0 +1,6 @@
+/* eslint-disable semi */
+export const createFilmCardTemplate = () => {
+  `<button class="films-list__show-more">
+    Show more
+  </button>`
+};
